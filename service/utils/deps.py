5f913fcf7f8bdabd3fb7c366727d894b690cea91
Cyril Perie <cyril.perie96@gmail.com>
2026-08-21 20:25:08 +0200

    :green_heart: Add an error when environnement variable is missing.

--- service/utils/deps.py
+++ service/utils/deps.py
@@ -6,6 +6,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL est absente de l'environnement")
+
 engine = create_engine(DATABASE_URL)
 
 def get_session():
