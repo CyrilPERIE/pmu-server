7e88bb66acd72f7f5b4defee62ed0b0ea95c5227
Cyril Perie <cyril.perie96@gmail.com>
2026-08-23 22:15:05 +0200

    :sparkles: Add some metrics.

--- models/metrics.py
+++ models/metrics.py
@@ -3,6 +3,12 @@ from sqlmodel import Field, SQLModel
 from sqlalchemy import Column, Enum
 from datetime import datetime
 
+'''
+TODO: Améliorer le modèle en ajoutant des colonnes
+- order: pour ordonner les courses plus facilement côté frontend
+- category ? - enum: Scraper, Paris, PMU, IA... - Pour filtrer plus facilement côté frontend
+'''
+
 class MetricType(str, PyEnum):
     COUNT = "count"
     DURATION = "duration"
--- service/metrics.py
+++ service/metrics.py
@@ -1,4 +1,6 @@
 from models.course import Course
+from models.programme import Programme
+from models.reunion import Reunion
 from service.utils.crud import upsert
 from service.utils.deps import get_session
 from sqlmodel import Session, select
@@ -14,9 +16,50 @@ def create_metric(metric: Metrics, session: Session) -> Metrics:
 def update_metrics() -> None:
     with get_session() as session:
         update_count_courses(session)
+        update_count_reunions(session)
+        update_count_programmes(session)
+        update_count_courses_over(session)
+        update_count_courses_incoming(session)
+        update_count_mean_courses_by_programme(session)
 
 def update_count_courses(session: Session) -> int:
     count = session.exec(select(func.count(Course.id))).one()
-    print(count)
-    metric = Metrics(type=MetricType.COUNT, value=count, name="count_courses")
+    metric = Metrics(type=MetricType.COUNT, value=count, name="Nombre de courses récupérées")
     return create_metric(metric, session)
+
+def update_count_programmes(session: Session) -> int:
+    count = session.exec(select(func.count(Programme.id))).one()
+    metric = Metrics(type=MetricType.COUNT, value=count, name="Nombre de programmes récupérés")
+    return create_metric(metric, session)
+
+def update_count_reunions(session: Session) -> int:
+    count = session.exec(select(func.count(Reunion.id))).one()
+    metric = Metrics(type=MetricType.COUNT, value=count, name="Nombre de réunions récupérées")
+    return create_metric(metric, session)
+
+def update_count_courses_over(session: Session) -> int:
+    count = session.exec(select(func.count(Course.id)).where(Course.is_over == True)).one()
+    metric = Metrics(type=MetricType.COUNT, value=count, name="Nombre de courses terminées")
+    return create_metric(metric, session)
+
+def update_count_courses_incoming(session: Session) -> int:
+    count = session.exec(select(func.count(Course.id)).where(Course.is_over == False)).one()
+    metric = Metrics(type=MetricType.COUNT, value=count, name="Nombre de courses en cours de récupération")
+    return create_metric(metric, session)
+
+def update_count_mean_courses_by_programme(session: Session) -> int:
+    courses_by_programme = (
+        select(
+            Reunion.programme_id,
+            func.count(Course.id).label("course_count")
+        )
+        .join(Course, Course.reunion_id == Reunion.id)
+        .group_by(Reunion.programme_id)
+        .subquery()
+    )
+    count = session.exec(
+        select(func.avg(courses_by_programme.c.course_count))
+    ).one()
+    metric = Metrics(type=MetricType.COUNT, value=count, name="Nombre moyen de courses par jour")
+    return create_metric(metric, session)
+
