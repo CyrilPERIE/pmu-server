c433e1830361530e0548f95542b4338ab97daa62
Cyril Perie <cyril.perie96@gmail.com>
2026-09-03 00:02:57 +0200

    :loud_sound: Configure root logger.

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,13 @@
 import logging
 
-def setup_logging():
+def setup_logging(level=logging.INFO):
+    # La fonction logging.basicConfig ne configure pas le root logger si un handler existe déjà,
+    # ce qui arrive souvent sur Railway ou d'autres environnements qui préconfigurent le logging.
+    # Pour forcer la configuration, il faut explicitement supprimer les handlers existants du root logger.
+    root_logger = logging.getLogger()
+    if root_logger.hasHandlers():
+        root_logger.handlers.clear()
     logging.basicConfig(
-        level=logging.INFO,
+        level=level,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
